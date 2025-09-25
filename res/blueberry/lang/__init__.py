fbb89568d487577947ec48e15a30c8f7114542ea
totally not a meme <theguyfromtheinternett@gmail.com>
2025-09-25 21:42:22 +0300

    pyinstaller spaghetti fix
    FOR SOME REASON it just doesn't work with star imports, oops, will probably have to reupload the build

--- res/blueberry/lang/__init__.py
+++ res/blueberry/lang/__init__.py
@@ -7,9 +7,11 @@ __all__ = [
     "ru",
 ]
 
-from . import *
+# from . import * # pyinstaller spaghetti refuses to work with this
+from . import en, ru
 
 
 langmap = {i: eval(i) for i in __all__}
 default = langmap["en"]
+
 
